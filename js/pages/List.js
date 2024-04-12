9d7d67b827768d443b5172f314a3a7ebbce4bbbe
PotatoDaMan <105178860+PotatoDaMan@users.noreply.github.com>
2024-04-12 08:45:24 -0700

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -99,29 +99,14 @@ export default {
                         </ol>
                     </template>
                     <h3>Submission Requirements</h3>
-                    <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                     <p>
+                        Achieved the record without using hacks. All rules from the Pointercrate Demon List apply, although exceptions can be made.
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        All submitter must be in the Trigonometry Sprint Cologne group chat for the records to be counted and uploaded.
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
-                    </p>
-                    <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
-                    </p>
-                    <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
-                    </p>
-                    <p>
-                        Do not use secret routes or bug routes
-                    </p>
-                    <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
-                    </p>
-                    <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Any demon completion must be Medium Demon or above to be added to the list, as long as it is legitimately completed.
                     </p>
                 </div>
             </div>
