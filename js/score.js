6434ee52e5961427c8580866898369cdf027181c
PotatoDaMan <105178860+PotatoDaMan@users.noreply.github.com>
2024-11-04 08:48:21 -0800

    Update score.js

--- js/score.js
+++ js/score.js
@@ -12,10 +12,10 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 100) {
+    if (rank > 75) {
         return 0;
     }
-    if (rank > 100 && percent < 100) {
+    if (rank > 75 && percent < 100) {
         return 0;
     }
 
