bec0e44bcc4438f8b53ae408736bab6172be3604
PotatoDaMan <105178860+PotatoDaMan@users.noreply.github.com>
2024-11-18 19:55:01 -0800

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,9 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
-import Packs from './pages/Packs.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
-    { path: '/packs', component: Packs },
 ];
